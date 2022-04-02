99c96f3c17837fc6524c9c551eb104cbcf79c2b3
EgeSythn <eseyith1@gmail.com>
2022-04-02 16:54:15 -0400

    courses recycler started, layout files added (need editing)

--- app/src/main/java/com/example/teame_hopreview/LoginActivity.java
+++ app/src/main/java/com/example/teame_hopreview/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
