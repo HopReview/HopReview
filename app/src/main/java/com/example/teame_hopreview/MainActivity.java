db0e075b0c3df972c5ff0ed3d9d24ad5e903c288
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-04-07 19:27:13 -0400

    add dbref to various fragments and main activity

--- app/src/main/java/com/example/teame_hopreview/MainActivity.java
+++ app/src/main/java/com/example/teame_hopreview/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     private FirebaseDatabase mydbase;
     private DatabaseReference dbRef;
 
+    // TODO: for testing only, remove later
+    private static final String TAG = "DB_REF: ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +49,34 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
 
         mydbase = FirebaseDatabase.getInstance();
         dbRef = mydbase.getReference();
+
+        // TODO: need to adjust for recycler CourseListFrag
+//        dbRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                long count = snapshot.getChildrenCount();
+//                Log.d(TAG, "Children count: " + count);
+//                Log.d(TAG, "Client count: " + snapshot.child("clients").getChildrenCount());
+//
+//                // need to recreate the mItems list somehow
+//                // another way is to use a FirebaseRecyclerView - see Sample Database code
+//
+//                mItems.clear();
+//                Iterable<DataSnapshot> clients = snapshot.child("clients").getChildren();
+//                for (DataSnapshot pair : clients) {
+//                    mItems.add(pair.getValue(CourseItem.class));
+//                }
+//                mAdapt.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
 
         getSupportFragmentManager().beginTransaction().add(R.id.container, this.home).commit();
 
